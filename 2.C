86ed8df5168f36531fb76827a6996554adcfecbb
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 20:34:30 -0300

    Create 2.C

---
+++ 2.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int num1=2;
+    int num2=6;
+    int soma=num1+num2;
+printf("a soma é:%i", soma);
+
+    return 0;
+}
