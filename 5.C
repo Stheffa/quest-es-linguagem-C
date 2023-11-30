a2a3be5a43a4206403db232fb771d104bed907fe
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 20:53:09 -0300

    Create 5.C

---
+++ 5.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int num1=5;
+    int num2=-4;
+    int soma=num1+num2;
+printf("a soma é:%i", soma);
+int subtracao=num1-num2;
+printf("a subtracao é:%i", subtracao);
+int multiplicacao= num1*num2;
+printf("a multiplicao é:%i", multiplicacao);
+int divisao=num1/num2;
+printf("a divisao é:%i", divisao);
+    return 0;
+}
