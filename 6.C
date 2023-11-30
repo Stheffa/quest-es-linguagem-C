f9badfc219a51b14ddd99c11c381f727ca1c8bee
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 20:57:14 -0300

    Create 6.C

---
+++ 6.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int num1=5;
+    int num2=-4;
+int divisao=num1/num2;
+printf("a divisao é:%i", divisao);
+    return 0;
+}
