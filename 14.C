3c8acbe0db4c6545163bcfd2c25517565cadb613
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 21:57:57 -0300

    Create 14.C

---
+++ 14.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int A=8;
+    int B=10;
+    int C=12;
+    int soma=(A*A)+(B*B)+(C*C);
+    printf("a soma dos quadrados é:%i", soma);
+    return 0;
+}
