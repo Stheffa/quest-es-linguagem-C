89210fe16d63eb0f4d5eb0c1b7c7d1b01153a3a4
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 21:21:50 -0300

    Create 8.C

---
+++ 8.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int r=10;
+    int pi=3;
+int area= pi*(r*r);
+printf("a area é:%i", area);
+int comprimento=2*pi*r;
+printf("o comprimento é:%i", comprimento);
+    return 0;
+}
