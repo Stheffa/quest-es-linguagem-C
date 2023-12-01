dbe7206183b7c6cc779d23df282aebfb9fbf416c
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 21:38:44 -0300

    Create 10.C

---
+++ 10.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int B=10;
+    int b=5;
+    int h=8;
+int area=((B+b)*h)/2;
+printf(" a area é:%i", area);
+    return 0;
+}
