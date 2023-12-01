22dad4ef6b32fe407b6f47711741a389353c7914
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 22:02:51 -0300

    Create 15.C

---
+++ 15.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int km=350;
+    int L=35;
+    int gasto= km/L;
+    printf("o gasto é:%i",gasto);
+    return 0;
+}
