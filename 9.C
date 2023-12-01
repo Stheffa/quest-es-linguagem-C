469e9832718b39f97935250300a334641c7f5d7c
Stheffany Magalhães Pinto <151779184+Stheffa@users.noreply.github.com>
2023-11-30 21:32:42 -0300

    Create 9.C

---
+++ 9.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    // Write C code here
+    printf("Hello world");
+    int r=5;
+    int pi=3;
+int volume=(4/3)*pi*(r*r*r);
+printf("o volume é:%i", volume);
+    return 0;
+}
